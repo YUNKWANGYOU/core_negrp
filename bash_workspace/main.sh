1ae3540b2d276d689f0abaf6831cb7905ee9b79f
yunkwangyou <>
2022-11-08 20:14:31 +0900

    .

--- bash_workspace/main.sh
+++ bash_workspace/main.sh
@@ -1,36 +1,55 @@
 #!/bin/bash
 
-chkNet.sh
-chkTracer.sh
+# Check the Network Access Fail Information
+# Test Ver : RHEL 7.1
+# 2022-10-23 yuns@sktelecom.com
 
 sysList='MME_01 MME_02 MME_06 MME_10'
 logDir='/home/cnems/SSNOC'
 errLog="$logDir/errLog.log"
-tgtServer='60.11.64.13/10003'
+tgtServer=''
 clearTime='0300'
 currentTime=$(date +"%H%M")
 
-function chkFile {
-        if [ -e "$1" ] && [ -s "$1" ] ; then echo "T" ; else echo "F" ; fi
-};
-
+declare -a priList_1
+declare -a priList_2
+declare -a secList_1
+declare -a secList_2
 cnt=0
 
 for sysName in $sysList
 do
-        while read line
-        do
-                if [ "${cnt}" == 0 ]
-                then
-                        cmd "$sysName" TEST-TRC-ROUTE:DIP="$line"; > res_main_${sysName}.log
-                        (( cnt = "${cnt}" + 1 ))
+        priList_1[$cnt]=$(awk '$3 ~ /172\./{ print $3 } ' input1_${sysName}.log)
+        priList_2[$cnt]=$(awk '$3 ~ /10\./{ print $3 } ' input1_${sysName}.log)
+        secList_1[$cnt]=$(awk '$2 ~ /172\./{ print $2 } ' input1_${sysName}.log)
+        secList_2[$cnt]=$(awk '$2 ~ /10\./{ print $2 } ' input1_${sysName}.log)
 
-                else
-			cmd "$sysName" TEST-TRC-ROUTE:DIP="$line"; >> res_main_${sysName}.log
-
-		fi
-
-        done < res_net_${sysName}.log
+        netList="$priList_1\n$priList_2\n$secList_1\n$secList_2"
+        
+        (( cnt = "${cnt}" + 1 ))
 done
+
+echo -e "$netList"
+
+# chkTracer.sh
+
+# cnt=0
+
+# for sysName in $sysList
+# do
+#         while read line
+#         do
+#                 if [ "${cnt}" == 0 ]
+#                 then
+#                         cmd "$sysName" TEST-TRC-ROUTE:DIP="$line"; > res_main_${sysName}.log
+#                         (( cnt = "${cnt}" + 1 ))
+
+#                 else
+# 			cmd "$sysName" TEST-TRC-ROUTE:DIP="$line"; >> res_main_${sysName}.log
+
+# 		fi
+
+#         done < res_net_${sysName}.log
+# done
 
 
