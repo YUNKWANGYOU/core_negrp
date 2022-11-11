9c3979a4700fc028f44407ae1e900d2638ecdcab
yunkwangyou <>
2022-11-11 14:56:54 +0900

    .

--- bash_workspace/chkTracert.sh
+++ bash_workspace/chkTracert.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# Check the Network Access Fail Information
-# Test Ver : RHEL 7.1
+# Save Trace Route
+# Test Ver : RHEL 8.2
 # 2022-10-23 yuns@sktelecom.com
 
-sysList=(MME_01 MME_02 MME_06 MME_10)
-logDir='/home/cnems/SSNOC'
-errLog="$logDir/errLog.log"
-tgtServer=''
-clearTime='0300'
-currentTime=$(date +"%H%M")
+# mkdir /home/cnems/tracert
+
+# crontab -e
+# 0 3 1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31 * /home/cnems/SSNOC/tracert/chkErrpoint.sh
+
+sysList=(MME_01)
+logDir='/home/cnems/SSNOC/tracert'
+errLog="$logDir/errLog_tracert.log"
+targetServer='*.*.*.*/*'
 
 # sysList에 속한 장비들의 RTRV-NE-STS; 수행하여 input1_${sysList[@]}.log로 저장하기
 for sysName in "${sysList[@]}"
@@ -17,32 +20,26 @@ do
         # 실제 CN_EMS에서는 아래 주석처리 된 두 줄의 커맨드 이용 예정
         echo "cmd ${sysName} RTRV-NE-STS;" 
         # input_tmp=$(cmd ${sysName} RTRV-NE-STS;)
-        # echo $input_tmp > input1_${sysName}_bk.log
+        # echo $input_tmp > /home/cnems/tracert/input_${sysName}.log
 done
 
 # 변수 선언
-declare -a priList_1
-declare -a priList_2
-declare -a secList_1
-declare -a secList_2
+declare -a priList
+declare -a secList
 
 cnt=0
 
 # RTRV-NE-STS; 결과 중 172. 또는 10.을 포함하는 연동 정보 가져오기
 for sysName in "${sysList[@]}"
 do
-        priList_1[$cnt]=$(awk '$3 ~ /172\./{ print $3 } ' input1_${sysName}.log)
-        priList_2[$cnt]=$(awk '$3 ~ /10\./{ print $3 } ' input1_${sysName}.log)
-        secList_1[$cnt]=$(awk '$2 ~ /172\./{ print $2 } ' input1_${sysName}.log)
-        secList_2[$cnt]=$(awk '$2 ~ /10\./{ print $2 } ' input1_${sysName}.log)
+        priList[$cnt]=$(awk '$2 ~ /\./ { print $2 }' input_${sysName}.log)
+        secList[$cnt]=$(awk '$3 ~ /\./ { print $3 }' input_${sysName}.log)
 
         # 합치기
-        netList[$cnt]="$priList_1 $priList_2 $secList_1 $secList_2"
+        netList[$cnt]="$priList $secList"
 
         (( cnt = "${cnt}" + 1 ))
 done
-
-# echo ${netList[1]}
 
 # 연동 IP 정보를 이용하여 TEST-TRC-ROUTE를 날리고 해당 결과를 res_$i_$netName.log 에 저장
 for i in $(seq $cnt)
